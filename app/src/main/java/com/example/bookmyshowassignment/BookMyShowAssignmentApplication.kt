2bcbf89b032d0e6802830d1860fc2d695194fc0e
SurajBichkunde39 <surajbichkunde@gmail.com>
2023-04-29 20:33:51 +0530

    Added initial setup for dependency injection - hilt

---
+++ app/src/main/java/com/example/bookmyshowassignment/BookMyShowAssignmentApplication.kt
@@ -0,0 +1,7 @@
+package com.example.bookmyshowassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookMyShowAssignmentApplication: Application()
