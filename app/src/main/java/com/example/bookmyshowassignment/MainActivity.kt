584e8a1a9f557570517e4b43e97730484370b413
SurajBichkunde39 <surajbichkunde@gmail.com>
2023-04-29 21:44:52 +0530

    Initial support for Movie details screen. - Add blank fragment for movie details screen. - Remove the default action bar.

--- app/src/main/java/com/example/bookmyshowassignment/MainActivity.kt
+++ app/src/main/java/com/example/bookmyshowassignment/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.bookmyshowassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bookmyshowassignment.ui.MovieDetailFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    MovieDetailFragment.newInstance(movieId = "453395")
+                )
+                .commit()
+        }
     }
 }
